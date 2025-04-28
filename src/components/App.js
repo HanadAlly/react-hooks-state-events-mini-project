8ce6b0604d30cc0cd65c2fad70e9089ce9b1a95c
HanadAlly <allyhanad404@gmail.com>
2025-04-28 06:55:56 +0300

    commit canges

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,47 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+ import React, { useState } from "react";
+ import CategoryFilter from "./CategoryFilter";
+ import NewTaskForm from "./NewTaskForm";
+ import TaskList from "./TaskList";
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+ import { CATEGORIES, TASKS } from "../data";
+ console.log("Here's the data you're working with");
+ console.log({ CATEGORIES, TASKS });
 
-function App() {
-  return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
-}
+ function App() {
+   const [selectedCategory, setSelectedCategory] = useState("All");
+   const [tasks, setTasks] = useState(TASKS);
 
-export default App;
+   const filteredTasks = tasks.filter((task) => {
+     return selectedCategory === "All" || task.category === selectedCategory;
+   });
+
+   const handleCategoryChange = (category) => {
+     setSelectedCategory(category);
+   };
+
+   const handleDeleteTask = (deletedTaskText) => {
+     setTasks(tasks.filter((task) => task.text !== deletedTaskText));
+   };
+
+   const handleTaskFormSubmit = (newTask) => {
+     setTasks([...tasks, newTask]);
+   };
+
+   return (
+     <div className="App">
+       <h2>My tasks</h2>
+       <CategoryFilter
+         categories={CATEGORIES}
+         selectedCategory={selectedCategory}
+         onCategoryChange={handleCategoryChange}
+       />
+       <NewTaskForm
+         categories={CATEGORIES}
+         onTaskFormSubmit={handleTaskFormSubmit}
+       />
+       <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} />
+     </div>
+   );
+ }
+
+ export default App;
